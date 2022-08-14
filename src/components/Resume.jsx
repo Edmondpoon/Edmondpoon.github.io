1b18572838a6a96ee06e1e9cd6c2436b6a1069c0
Edmond Poon <edpoon02@gmail.com>
2022-08-13 23:32:23 -0700

    updated resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -4,8 +4,9 @@ import {Document, Page, pdfjs} from 'react-pdf';
 import './Resume.css';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/
-  ${pdfjs.version}/pdf.worker.js`;
+pdfjs.GlobalWorkerOptions.workerSrc =
+  `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
+
 
 const options = {
   cMapUrl: 'cmaps/',
